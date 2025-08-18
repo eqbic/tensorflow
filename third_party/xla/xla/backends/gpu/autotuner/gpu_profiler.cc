openxla/xla

Expose ProgramShape from Executable even when HloModule is not available
PiperOrigin-RevId: 794105854

xla/backends/gpu/autotuner/gpu_profiler.cc
@@ -91,9 +91,10 @@ absl::StatusOr<std::unique_ptr<InputBuffers>> GpuProfiler::CreateInputBuffers(
   }
   TF_ASSIGN_OR_RETURN(
       RedzoneBuffers buffers,
-      RedzoneBuffers::FromComputation(
-          *executable->module().entry_computation(), allocator_.get(),
-          stream_.get(), RedzoneBuffers::BuffersToCreate::kAllInputs,
+      RedzoneBuffers::FromProgramShape(
+          executable->entry_computation_layout().ComputeProgramShape(),
+          allocator_.get(), stream_.get(),
+          RedzoneBuffers::BuffersToCreate::kAllInputs,
           options_.should_init_buffers,
           /*should_check_correctness=*/true, options_.redzone_padding_bytes));
   auto gpu_buffers = std::make_unique<GpuInputBuffers>();

xla/pjrt/pjrt_stream_executor_client.cc
@@ -2163,8 +2163,8 @@ PjRtStreamExecutorLoadedExecutable::PjRtStreamExecutorLoadedExecutable(
   executables_.reserve(executables.size());
   tsl::Fprint128 fingerprint = tsl::Fingerprint128(fingerprint_);
   for (auto& executable : executables) {
-    const auto& computation_layout =
-        executable->executable()->module().entry_computation_layout();
+    ComputationLayout computation_layout =
+        executable->executable()->entry_computation_layout();
     std::vector<Shape> parameter_shapes;
     parameter_shapes.reserve(computation_layout.parameter_count());
     for (int i = 0; i < computation_layout.parameter_count(); ++i) {
@@ -2178,6 +2178,9 @@ PjRtStreamExecutorLoadedExecutable::PjRtStreamExecutorLoadedExecutable(
     executables_.emplace_back(std::move(executable));
     on_device_executable_parameter_shapes_.push_back(
         std::move(parameter_shapes));
+  }
+  if (!executables_.empty()) {
+    name_ = executables_.front()->executable()->name();
   }
   fingerprint_ = absl::StrCat(fingerprint.low64, fingerprint.high64);
 
@@ -2233,12 +2236,7 @@ absl::Status PjRtStreamExecutorLoadedExecutable::SetUpDonation(
 }
 
 absl::string_view PjRtStreamExecutorLoadedExecutable::name() const {
-  Executable* executable = executables_[0]->executable();
-  if (executable->has_module()) {
-    return executable->module().name();
-  } else {
-    return "<unknown executable>";
-  }
+  return name_;
 }
 
 absl::Span<int const>
@@ -2762,10 +2760,9 @@ PjRtStreamExecutorLoadedExecutable::EnqueueExecution(
           << ", run_id=" << run_options.run_id().ToInt();
 
   if (VLOG_IS_ON(2)) {
-    auto executable_name =
-        executables_[executable_idx]->executable()->module().name();
     absl::Status host_callback_status = run_options.stream()->DoHostCallback(
-        [executable_name, launch_id(run_options.run_id().ToInt()), device]() {
+        [executable_name = name_, launch_id(run_options.run_id().ToInt()),
+         device]() {
           VLOG(2) << "Start device execution for " << executable_name
                   << ", launch_id: " << launch_id
                   << ", device: " << device->DebugString();
@@ -2773,7 +2770,7 @@ PjRtStreamExecutorLoadedExecutable::EnqueueExecution(
     if (!host_callback_status.ok()) {
       LOG(WARNING)
           << "Failed to do host callback for start device execution for "
-          << executable_name << ", status = " << host_callback_status;
+          << name_ << ", status = " << host_callback_status;
     }
   }
 
@@ -2782,10 +2779,9 @@ PjRtStreamExecutorLoadedExecutable::EnqueueExecution(
                         std::move(execution_inputs), run_options);
 
   if (VLOG_IS_ON(2)) {
-    auto executable_name =
-        executables_[executable_idx]->executable()->module().name();
     absl::Status host_callback_status = run_options.stream()->DoHostCallback(
-        [executable_name, launch_id(run_options.run_id().ToInt()), device]() {
+        [executable_name = name_, launch_id(run_options.run_id().ToInt()),
+         device]() {
           VLOG(2) << "Finish device execution for " << executable_name
                   << ", launch_id: " << launch_id
                   << ", device: " << device->DebugString();
@@ -2793,7 +2789,7 @@ PjRtStreamExecutorLoadedExecutable::EnqueueExecution(
     if (!host_callback_status.ok()) {
       LOG(WARNING)
           << "Failed to do host callback for start device execution for "
-          << executable_name << ", status = " << host_callback_status;
+          << name_ << ", status = " << host_callback_status;
     }
   }
 

xla/service/gpu/gpu_executable.h
@@ -37,6 +37,7 @@ limitations under the License.
 #include "xla/hlo/ir/hlo_input_output_alias_config.h"
 #include "xla/hlo/ir/hlo_module.h"
 #include "xla/service/buffer_assignment.h"
+#include "xla/service/computation_layout.h"
 #include "xla/service/executable.h"
 #include "xla/service/gpu/alias_info.h"
 #include "xla/service/gpu/buffer_allocations.h"
@@ -102,7 +103,7 @@ class GpuExecutable : public Executable {
     std::vector<ConstantInfo> constants;
     absl::flat_hash_map<ShapeIndex, OutputInfo> output_info;
     std::string module_name;
-    xla::Shape output_shape;
+    ProgramShape program_shape;
     std::optional<std::vector<BufferAllocation>> mlir_allocations;
     std::unique_ptr<const BufferAssignment> buffer_assignment;
     std::unique_ptr<GpuAliasInfo> alias_info;
@@ -121,10 +122,14 @@ class GpuExecutable : public Executable {
 
   const std::string& module_name() const { return module_name_; }
 
-  const xla::Shape& output_shape() const { return output_shape_; }
+  xla::Shape result_shape() const override { return program_shape_.result(); }
 
   const absl::flat_hash_map<ShapeIndex, OutputInfo>& output_info() const {
     return output_info_;
+  }
+
+  ComputationLayout entry_computation_layout() const override {
+    return ComputationLayout(program_shape_, /*ignore_layouts=*/false);
   }
 
   // This should be called before ExecuteOnStream.
@@ -265,7 +270,7 @@ class GpuExecutable : public Executable {
 
   std::string module_name_;
 
-  xla::Shape output_shape_;
+  ProgramShape program_shape_;
 
   // The allocations_ object contains allocations that **may** be used to
   // provide information for allocating memory for every output/temp buffer.

xla/service/gpu/gpu_executable_test.cc
@@ -15,17 +15,24 @@ limitations under the License.
 
 #include "xla/service/gpu/gpu_executable.h"
 
+#include <memory>
 #include <optional>
+#include <utility>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "xla/backends/gpu/runtime/sequential_thunk.h"
+#include "xla/backends/gpu/runtime/thunk.h"
 #include "xla/hlo/ir/hlo_input_output_alias_config.h"
+#include "xla/shape_layout.h"
 #include "xla/shape_util.h"
 #include "xla/tsl/platform/status_matchers.h"
+#include "xla/tsl/platform/statusor.h"
 #include "xla/tsl/util/proto/proto_matchers.h"
 
 namespace xla::gpu {
 namespace {
+using ::testing::ElementsAre;
 using ::tsl::proto_testing::EqualsProto;
 using ::tsl::testing::IsOkAndHolds;
 
@@ -72,6 +79,24 @@ TEST(GpuExecutableTest, OuputInfoToAndFromProto) {
               )pb"));
   EXPECT_THAT(GpuExecutable::OutputInfo::FromProto(output_info2.ToProto()),
               IsOkAndHolds(output_info2));
+}
+
+TEST(GpuExecutableTest, EntryComputationLayout) {
+  GpuExecutable::Params params;
+  params.module_name = "test_module";
+  params.program_shape.AddParameter(ShapeUtil::MakeShape(F32, {1, 2, 3}), "p0");
+  params.program_shape.AddParameter(ShapeUtil::MakeShape(U8, {1}), "p1");
+  *params.program_shape.mutable_result() = ShapeUtil::MakeShape(F64, {2});
+  params.executable =
+      std::make_unique<SequentialThunk>(Thunk::ThunkInfo{}, ThunkSequence{});
+
+  TF_ASSERT_OK_AND_ASSIGN(std::unique_ptr<GpuExecutable> executable,
+                          GpuExecutable::Create(std::move(params)));
+  EXPECT_THAT(executable->entry_computation_layout().parameter_layouts(),
+              ElementsAre(ShapeLayout(ShapeUtil::MakeShape(F32, {1, 2, 3})),
+                          ShapeLayout(ShapeUtil::MakeShape(U8, {1}))));
+  EXPECT_EQ(executable->entry_computation_layout().result_layout(),
+            ShapeLayout(ShapeUtil::MakeShape(F64, {2})));
 }
 
 }  // namespace

eqbic/tensorflow

third_party/xla/xla/backends/gpu/autotuner/gpu_profiler.cc
@@ -91,9 +91,10 @@ absl::StatusOr<std::unique_ptr<InputBuffers>> GpuProfiler::CreateInputBuffers(
   }
   TF_ASSIGN_OR_RETURN(
       RedzoneBuffers buffers,
-      RedzoneBuffers::FromComputation(
-          *executable->module().entry_computation(), allocator_.get(),
-          stream_.get(), RedzoneBuffers::BuffersToCreate::kAllInputs,
+      RedzoneBuffers::FromProgramShape(
+          executable->compute_computation_layout().ComputeProgramShape(),
+          allocator_.get(), stream_.get(),
+          RedzoneBuffers::BuffersToCreate::kAllInputs,
           options_.should_init_buffers,
           /*should_check_correctness=*/true, options_.redzone_padding_bytes));
   auto gpu_buffers = std::make_unique<GpuInputBuffers>();

third_party/xla/xla/pjrt/pjrt_stream_executor_client.cc
@@ -2177,8 +2177,8 @@ PjRtStreamExecutorLoadedExecutable::PjRtStreamExecutorLoadedExecutable(
   executables_.reserve(executables.size());
   tsl::Fprint128 fingerprint = tsl::Fingerprint128(fingerprint_);
   for (auto& executable : executables) {
-    const auto& computation_layout =
-        executable->executable()->module().entry_computation_layout();
+    ComputationLayout computation_layout =
+        executable->executable()->compute_computation_layout();
     std::vector<Shape> parameter_shapes;
     parameter_shapes.reserve(computation_layout.parameter_count());
     for (int i = 0; i < computation_layout.parameter_count(); ++i) {
@@ -2192,6 +2192,9 @@ PjRtStreamExecutorLoadedExecutable::PjRtStreamExecutorLoadedExecutable(
     executables_.emplace_back(std::move(executable));
     on_device_executable_parameter_shapes_.push_back(
         std::move(parameter_shapes));
+  }
+  if (!executables_.empty()) {
+    name_ = executables_.front()->executable()->name();
   }
   fingerprint_ = absl::StrCat(fingerprint.low64, fingerprint.high64);
 
@@ -2247,12 +2250,7 @@ absl::Status PjRtStreamExecutorLoadedExecutable::SetUpDonation(
 }
 
 absl::string_view PjRtStreamExecutorLoadedExecutable::name() const {
-  Executable* executable = executables_[0]->executable();
-  if (executable->has_module()) {
-    return executable->module().name();
-  } else {
-    return "<unknown executable>";
-  }
+  return name_;
 }
 
 absl::Span<int const>
@@ -2776,10 +2774,9 @@ PjRtStreamExecutorLoadedExecutable::EnqueueExecution(
           << ", run_id=" << run_options.run_id().ToInt();
 
   if (VLOG_IS_ON(2)) {
-    auto executable_name =
-        executables_[executable_idx]->executable()->module().name();
     absl::Status host_callback_status = run_options.stream()->DoHostCallback(
-        [executable_name, launch_id(run_options.run_id().ToInt()), device]() {
+        [executable_name = name_, launch_id(run_options.run_id().ToInt()),
+         device]() {
           VLOG(2) << "Start device execution for " << executable_name
                   << ", launch_id: " << launch_id
                   << ", device: " << device->DebugString();
@@ -2787,7 +2784,7 @@ PjRtStreamExecutorLoadedExecutable::EnqueueExecution(
     if (!host_callback_status.ok()) {
       LOG(WARNING)
           << "Failed to do host callback for start device execution for "
-          << executable_name << ", status = " << host_callback_status;
+          << name_ << ", status = " << host_callback_status;
     }
   }
 
@@ -2796,10 +2793,9 @@ PjRtStreamExecutorLoadedExecutable::EnqueueExecution(
                         std::move(execution_inputs), run_options);
 
   if (VLOG_IS_ON(2)) {
-    auto executable_name =
-        executables_[executable_idx]->executable()->module().name();
     absl::Status host_callback_status = run_options.stream()->DoHostCallback(
-        [executable_name, launch_id(run_options.run_id().ToInt()), device]() {
+        [executable_name = name_, launch_id(run_options.run_id().ToInt()),
+         device]() {
           VLOG(2) << "Finish device execution for " << executable_name
                   << ", launch_id: " << launch_id
                   << ", device: " << device->DebugString();
@@ -2807,7 +2803,7 @@ PjRtStreamExecutorLoadedExecutable::EnqueueExecution(
     if (!host_callback_status.ok()) {
       LOG(WARNING)
           << "Failed to do host callback for start device execution for "
-          << executable_name << ", status = " << host_callback_status;
+          << name_ << ", status = " << host_callback_status;
     }
   }
 

third_party/xla/xla/service/gpu/gpu_executable.h
@@ -37,6 +37,7 @@ limitations under the License.
 #include "xla/hlo/ir/hlo_input_output_alias_config.h"
 #include "xla/hlo/ir/hlo_module.h"
 #include "xla/service/buffer_assignment.h"
+#include "xla/service/computation_layout.h"
 #include "xla/service/executable.h"
 #include "xla/service/gpu/alias_info.h"
 #include "xla/service/gpu/buffer_allocations.h"
@@ -102,7 +103,7 @@ class GpuExecutable : public Executable {
     std::vector<ConstantInfo> constants;
     absl::flat_hash_map<ShapeIndex, OutputInfo> output_info;
     std::string module_name;
-    xla::Shape output_shape;
+    ProgramShape program_shape;
     std::optional<std::vector<BufferAllocation>> mlir_allocations;
     std::unique_ptr<const BufferAssignment> buffer_assignment;
     std::unique_ptr<GpuAliasInfo> alias_info;
@@ -121,10 +122,14 @@ class GpuExecutable : public Executable {
 
   const std::string& module_name() const { return module_name_; }
 
-  const xla::Shape& output_shape() const { return output_shape_; }
+  xla::Shape result_shape() const override { return program_shape_.result(); }
 
   const absl::flat_hash_map<ShapeIndex, OutputInfo>& output_info() const {
     return output_info_;
+  }
+
+  ComputationLayout compute_computation_layout() const override {
+    return ComputationLayout(program_shape_, /*ignore_layouts=*/false);
   }
 
   // This should be called before ExecuteOnStream.
@@ -265,7 +270,7 @@ class GpuExecutable : public Executable {
 
   std::string module_name_;
 
-  xla::Shape output_shape_;
+  ProgramShape program_shape_;
 
   // The allocations_ object contains allocations that **may** be used to
   // provide information for allocating memory for every output/temp buffer.

third_party/xla/xla/service/gpu/gpu_executable_test.cc
@@ -15,17 +15,24 @@ limitations under the License.
 
 #include "xla/service/gpu/gpu_executable.h"
 
+#include <memory>
 #include <optional>
+#include <utility>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "xla/backends/gpu/runtime/sequential_thunk.h"
+#include "xla/backends/gpu/runtime/thunk.h"
 #include "xla/hlo/ir/hlo_input_output_alias_config.h"
+#include "xla/shape_layout.h"
 #include "xla/shape_util.h"
 #include "xla/tsl/platform/status_matchers.h"
+#include "xla/tsl/platform/statusor.h"
 #include "xla/tsl/util/proto/proto_matchers.h"
 
 namespace xla::gpu {
 namespace {
+using ::testing::ElementsAre;
 using ::tsl::proto_testing::EqualsProto;
 using ::tsl::testing::IsOkAndHolds;
 
@@ -72,6 +79,24 @@ TEST(GpuExecutableTest, OuputInfoToAndFromProto) {
               )pb"));
   EXPECT_THAT(GpuExecutable::OutputInfo::FromProto(output_info2.ToProto()),
               absl_testing::IsOkAndHolds(output_info2));
+}
+
+TEST(GpuExecutableTest, ComputeComputationLayout) {
+  GpuExecutable::Params params;
+  params.module_name = "test_module";
+  params.program_shape.AddParameter(ShapeUtil::MakeShape(F32, {1, 2, 3}), "p0");
+  params.program_shape.AddParameter(ShapeUtil::MakeShape(U8, {1}), "p1");
+  *params.program_shape.mutable_result() = ShapeUtil::MakeShape(F64, {2});
+  params.executable =
+      std::make_unique<SequentialThunk>(Thunk::ThunkInfo{}, ThunkSequence{});
+
+  TF_ASSERT_OK_AND_ASSIGN(std::unique_ptr<GpuExecutable> executable,
+                          GpuExecutable::Create(std::move(params)));
+  EXPECT_THAT(executable->compute_computation_layout().parameter_layouts(),
+              ElementsAre(ShapeLayout(ShapeUtil::MakeShape(F32, {1, 2, 3})),
+                          ShapeLayout(ShapeUtil::MakeShape(U8, {1}))));
+  EXPECT_EQ(executable->compute_computation_layout().result_layout(),
+            ShapeLayout(ShapeUtil::MakeShape(F64, {2})));
 }
 
 }  // namespace