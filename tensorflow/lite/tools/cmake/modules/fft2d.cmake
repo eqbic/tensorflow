583d876e5768cc9a0294296410936c12286597d5
Erich Querner <51314413+eqbic@users.noreply.github.com>
2025-08-19 09:52:28 +0200

    Update fft2d.cmake
    use http instead of https as a workaround

--- tensorflow/lite/tools/cmake/modules/fft2d.cmake
+++ tensorflow/lite/tools/cmake/modules/fft2d.cmake
@@ -21,7 +21,7 @@ include(OverridableFetchContent)
 
 OverridableFetchContent_Declare(
   fft2d
-  URL https://storage.googleapis.com/mirror.tensorflow.org/github.com/petewarden/OouraFFT/archive/v1.0.tar.gz
+  URL http://storage.googleapis.com/mirror.tensorflow.org/github.com/petewarden/OouraFFT/archive/v1.0.tar.gz
   # Sync with tensorflow/workspace2.bzl
   URL_HASH SHA256=5f4dabc2ae21e1f537425d58a49cdca1c49ea11db0d6271e2a4b27e9697548eb
   SOURCE_DIR "${CMAKE_BINARY_DIR}/fft2d"
